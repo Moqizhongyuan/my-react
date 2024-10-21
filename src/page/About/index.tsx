d5135a8b7c505a7c628e8c546e36998d859c0e22
Zhongyuan Yu <zhongyuanyu@ZhongyuandeMacBook-Air.local>
2024-10-21 21:37:50 +0800

    flex

--- src/page/About/index.tsx
+++ src/page/About/index.tsx
@@ -5,6 +5,15 @@ export default function About() {
     <div className={style.about}>
       这是一个about页面
       <h1>这是about中的一个h1标题</h1>
+      <div className={style.bg1}>
+        <div className={style.left}>
+          <div className={style.item}></div>
+          <div className={style.item}></div>
+          <div className={style.item}></div>
+          <div className={style.item}></div>
+        </div>
+        <div className={style.right}></div>
+      </div>
     </div>
   );
 }
